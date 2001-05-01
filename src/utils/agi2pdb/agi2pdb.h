5dfcf77380c5a8a2f8e6bb7505123366f6a6b713
Claudio Matsuoka <cmatsuoka@gmail.com>
2001-05-01 20:04:59 +0000

    Re-indented comments in the pdb header structures. Now it's easier to read in 80-column terminals.

--- src/utils/agi2pdb/agi2pdb.h
+++ src/utils/agi2pdb/agi2pdb.h
@@ -116,41 +116,66 @@ extern void write_dword(UINT8 *mem, UINT32 w);
 
 struct pdb_pdbheader
 {
-	UINT8 name[32];				// String - This is the name of the database on the PalmPilot device.
-								//  It need not match the name of the PDB file in the environment in which it is created.
-	UINT16 fileAttributes;		// Attributes of the pdb file.
-								//  0x0002 Read-Only
-								//  0x0004 Dirty AppInfoArea
-								//  0x0008 Backup this database (i.e. no conduit exists)
-								//  0x0010 (16 decimal) Okay to install newer over existing copy, if present on PalmPilot
-								//  0x0020 (32 decimal) Force the PalmPilot to reset after this database is installed
-	UINT16 version;				// Defined by the application.
-	UINT32 creationDate;		// Expressed as the number of seconds since January 1, 1904.
-								//  The database will not install if this value is zero. (PalmOS 1.0.6)
-	UINT32 modificationDate;	// Expressed as the number of seconds since January 1, 1904.
-								//  The database will not install if this value is zero. (PalmOS 1.0.6)
-	UINT32 lastBackupDate;		// Expressed as the number of seconds since January 1, 1904.
-								//  This can be left at zero and the database will install.
-	UINT32 modificationNumber;	// Set to zero.
-	UINT32 appInfoArea;			// The byte number in the PDB file (counting from zero) at which the AppInfoArea is located.
-								//  This must be the first entry in the Data portion of the PDB file.
-								//  If this database does not have an AppInfoArea, set this value to zero.
-	UINT32 sortInfoArea;		// The byte number in the PDB file (counting from zero) at which the SortInfoArea is
-								//  located. This must be placed immediately after the AppInfoArea, if one exists,
-								//  within the Data portion of the PDB file. If this database does not have a SortInfoArea,
-								//  set this value to zero. Do not use this. See Note C below for further details.
-	UINT8 databaseType[4];		// String - Set this to the desired value. Generally it should match the Database Type
-								//  used by the corresponding application This is 4 characters long and does not have a
-								//  terminating null.
-	UINT8 creatorID[4];			// String - Set this to the desired value. Generally it should match the Creator ID used
-								//  by the corresponding application. In all cases, you should always register your
-								//  Creator ID before using it. This is 4 characters long and does not have a terminating
-								//  null.
-	UINT32 uniqueIDSeed;		// This is used to generate the Unique ID number of subsequent records. Generally,
-								//  this should be set to zero.
-	UINT32 nextRecordListID;	// Set this to zero. (This does not appear to be used, but that has not been verified by
-								//  a third party.)
-	UINT16 numberOfRecords;		// This contains the number of records
+	UINT8 name[32];		// String - This is the name of the database
+				//  on the PalmPilot device.
+				//  It need not match the name of the PDB file
+				//  in the environment in which it is created.
+	UINT16 fileAttributes;	// Attributes of the pdb file.
+				//  0x0002 Read-Only
+				//  0x0004 Dirty AppInfoArea
+				//  0x0008 Backup this database
+				//         (i.e. no conduit exists)
+				//  0x0010 Okay to install newer over existing
+				//         copy, if present on PalmPilot
+				//  0x0020 Force the PalmPilot to reset after
+				//         this database is installed
+	UINT16 version;		// Defined by the application.
+	UINT32 creationDate;	// Expressed as the number of seconds since
+				//  January 1, 1904. The database will not
+				//  install if this value is zero. (PalmOS
+				//  1.0.6)
+	UINT32 modificationDate;// Expressed as the number of seconds since
+				//  January 1, 1904.
+				//  The database will not install if this
+				//  value is zero. (PalmOS 1.0.6)
+	UINT32 lastBackupDate;	// Expressed as the number of seconds since
+				//  January 1, 1904. This can be left at zero
+				//  and the database will install.
+	UINT32 modificationNumber;// Set to zero.
+	UINT32 appInfoArea;	// The byte number in the PDB file (counting
+				//  from zero) at which the AppInfoArea is
+				//  located. This must be the first entry
+				//  in the Data portion of the PDB file.
+				//  If this database does not have an
+				//  AppInfoArea, set this value to zero.
+	UINT32 sortInfoArea;	// The byte number in the PDB file (counting
+				//  from zero) at which the SortInfoArea is
+				//  located. This must be placed immediately
+				//  after the AppInfoArea, if one exists,
+				//  within the Data portion of the PDB file.
+				//  If this database does not have a
+				//  SortInfoArea, set this value to zero.
+				//  Do not use this. See Note C below for
+				//  further details.
+	UINT8 databaseType[4];	// String - Set this to the desired value.
+				//  Generally it should match the Database Type
+				//  used by the corresponding application.
+				//  This is 4 characters long and does not have
+				//  a terminating null.
+	UINT8 creatorID[4];	// String - Set this to the desired value.
+				//  Generally it should match the Creator ID
+				//  used by the corresponding application. In
+				//  all cases, you should always register your
+				//  Creator ID before using it. This is 4
+				//  characters long and does not have a
+				//  terminating null.
+	UINT32 uniqueIDSeed;	// This is used to generate the Unique ID
+				//  number of subsequent records. Generally,
+				//  this should be set to zero.
+	UINT32 nextRecordListID;// Set this to zero. (This does not appear
+				//  to be used, but that has not been
+				//  verified by a third party.)
+	UINT16 numberOfRecords;	// This contains the number of records
 }
 #ifdef __GNUC__
 __attribute__((packed))
@@ -158,14 +183,18 @@ __attribute__((packed))
 ;
 
 struct pdb_recheader{
-	UINT32 recordDataOffset;	// The byte number in the PDB file (counting from zero) at which the record is located.
-	UINT8 recordAttributes;		// The records attributes.
- 								//  0x10 (16 decimal) Secret record bit.
-								//  0x20 (32 decimal) Record in use (busy bit).
-								//  0x40 (64 decimal) Dirty record bit.
-								//  0x80 (128, unsigned decimal) Delete record on next HotSync.
-								//  The least significant four bits are used to represent the category values.
-	UINT8	uniqueID[3];		// Set this to zero and do not try to second-guess what PalmOS will do with this value.
+	UINT32 recordDataOffset;// The byte number in the PDB file (counting
+				//  from zero) at which the record is located.
+	UINT8 recordAttributes;	// The records attributes.
+ 				//  0x10 Secret record bit.
+				//  0x20 Record in use (busy bit).
+				//  0x40 Dirty record bit.
+				//  0x80 Delete record on next HotSync.
+				//  The least significant four bits are used
+				//   to represent the category values.
+	UINT8	uniqueID[3];	// Set this to zero and do not try to
+				//  second-guess what PalmOS will do with
+				//  this value.
 }
 #ifdef __GNUC__
 __attribute__((packed))
