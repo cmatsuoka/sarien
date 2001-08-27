8bf13bc92249a66a53f4a9362d36588b30cc3bd2
Claudio Matsuoka <cmatsuoka@gmail.com>
2001-08-27 17:00:22 +0000

    Added mixing and double-buffering to the AHI driver.

--- src/sound/amiga/sound_ahi.c
+++ src/sound/amiga/sound_ahi.c
@@ -10,7 +10,9 @@
 
 #include <devices/ahi.h>
 #include <exec/exec.h>
+#if 0
 #include <proto/ahi.h>
+#endif
 #include <proto/dos.h>
 #include <proto/exec.h>
 #include "sarien.h"
@@ -28,16 +30,57 @@ static struct sound_driver sound_ahi = {
 
 
 static struct Library      *AHIBase;
-static struct MsgPort      *AHImp     = NULL;
-static struct AHIRequest   *AHIio     = NULL;
-static BYTE                 AHIDevice = -1;
-static struct AHIAudioCtrl *actrl     = NULL;
+static struct MsgPort      *AHImp    = NULL;
+static struct AHIRequest   *AHIio    = NULL;
+static BYTE                AHIDevice = -1;
+static struct AHIAudioCtrl *actrl    = NULL;
+static BOOL                DBflag    = FALSE;	/**< double buffer flag */
+static ULONG               BUFFER_SIZE = NULL;
 
+
+
+static struct AHISampleInfo Sample0 = {
+	AHIST_M16S,
+	NULL,
+	NULL,
+};
+ 
+static struct AHISampleInfo Sample1 = {
+	AHIST_M16S,
+	NULL,
+	NULL,
+};
+ 
+__asm __saveds ULONG SoundFunc (
+	register __a0 struct Hook *hook,
+	register __a2 struct AHIAudioCtrl *actrl,
+	register __a1 struct AHISoundMessage *smsg)
+{
+	if (DBflag = !DBflag)		/* Flip and test */
+		AHI_SetSound (0, 1, 0, 0, actrl, NULL);
+	else
+		AHI_SetSound (0, 0, 0, 0, actrl, NULL);
+
+	play_sound ();
+	mix_sound ();
+	/* FIXME: copy sound to buffer */
+	
+	Signal (actrl->ahiac_UserData, (1L<<signal));
+	return NULL;
+}
+
+static struct Hook SoundHook = {
+  0,0,
+  (ULONG (* )()) SoundFunc,
+  NULL,
+  NULL,
+};
 
 void __init_sound ()
 {
 	snd = &sound_ahi;
 }
+
 
 static int ahi_init_sound (SINT16 *buffer)
 {
@@ -67,6 +110,23 @@ static int ahi_init_sound (SINT16 *buffer)
 
 	AHIBase = (struct Library *)AHIio->ahir_Std.io_Device;
 
+	actrl = AHI_AllocAudio(
+		AHIA_AudioID,   AHI_DEFAULT_ID,
+		AHIA_MixFreq,   22050,
+		AHIA_Channels,  1,
+		AHIA_Sounds,    2,
+		AHIA_SoundFunc, &SoundHook,
+		AHIA_UserData,	FindTask (NULL),
+		TAG_DONE);
+
+	Sample0.ahisi_Length = BUFFER_SIZE;
+	Sample0.ahisi_Address = AllocVec (BUFFER_SIZE * 2,
+		MEMF_PUBLIC | MEMF_CLEAR);
+
+	Sample1.ahisi_Length = BUFFER_SIZE;
+	Sample1.ahisi_Address = AllocVec (BUFFER_SIZE * 2,
+		MEMF_PUBLIC | MEMF_CLEAR);
+
 	return 0;
 
 err3:
@@ -86,201 +146,7 @@ static void ahi_close_sound ()
 	CloseDevice((struct IORequest *)AHIio);
 	DeleteIORequest ((struct IORequest *)AHIio);
 	DeleteMsgPort (AHImp);
+	FreeVec (Sample1);
+	FreeVec (Sample0);
 }
 
-#if 0
-
-
-#define MAXSAMPLES 16
-
-#define INT_FREQ   50
-
-APTR samples[MAXSAMPLES] = { 0 };
-
-LONG mixfreq = 0;
-
-/* Prototypes */
-
-
-/******************************************************************************
-** PlayerFunc *****************************************************************
-******************************************************************************/
-
-__asm __interrupt __saveds static void PlayerFunc(
-    register __a0 struct Hook *hook,
-    register __a2 struct AHIAudioCtrl *actrl,
-    register __a1 APTR ignored) {
-
-  int i;
-  
-  for(i = 0; i < CHANNELS; i++) {
-
-    if(channelstate[i].FadeVolume) {
-
-      channelstate[i].Volume = (channelstate[i].Volume * 90) / 100; // Fade volume
-
-      if(channelstate[i].Volume == 0) {
-        channelstate[i].FadeVolume = FALSE;
-      }
-
-      AHI_SetVol(i, channelstate[i].Volume, channelstate[i].Position,
-          actrl, AHISF_IMM);
-    }
-  }
-  return;
-}
-
-struct Hook PlayerHook = {
-  0,0,
-  (ULONG (* )()) PlayerFunc,
-  NULL,
-  NULL,
-};
-
-/* Ask user for an audio mode and allocate it */
-
-BOOL AllocAudio(void) {
-  struct AHIAudioModeRequester *req;
-  BOOL   rc = FALSE;
-
-  req = AHI_AllocAudioRequest(
-      AHIR_PubScreenName, NULL,
-      AHIR_TitleText,     "Select a mode and rate",
-      AHIR_DoMixFreq,     TRUE,
-      TAG_DONE);
-
-  if(req) {
-    if(AHI_AudioRequest(req, TAG_DONE)) {
-      actrl = AHI_AllocAudio(
-          AHIA_AudioID,         req->ahiam_AudioID,
-          AHIA_MixFreq,         req->ahiam_MixFreq,
-          AHIA_Channels,        CHANNELS,
-          AHIA_Sounds,          MAXSAMPLES,
-          AHIA_PlayerFunc,      &PlayerHook,
-          AHIA_PlayerFreq,      INT_FREQ<<16,
-          AHIA_MinPlayerFreq,   INT_FREQ<<16,
-          AHIA_MaxPlayerFreq,   INT_FREQ<<16,
-          TAG_DONE);
-      if(actrl) {
-        // Get real mixing frequency
-        AHI_ControlAudio(actrl, AHIC_MixFreq_Query, &mixfreq, TAG_DONE);
-        rc = TRUE;
-      }
-    }
-    AHI_FreeAudioRequest(req);
-  }
-  return rc;
-}
-
-
-/******************************************************************************
-**** LoadSample ***************************************************************
-******************************************************************************/
-
-/* Load a (raw) 8 or 16 bit sample from disk. The sample ID is returned
-   (or AHI_NOSOUND on error). */
-
-UWORD LoadSample(char *filename, ULONG type) {
-  struct AHISampleInfo sample;
-  APTR *samplearray = samples;
-  UWORD id = 0, rc = AHI_NOSOUND;
-  BPTR file;
-
-  // Find a free sample slot
-
-  while(*samplearray) {
-    id++;
-    samplearray++;
-    if(id >= MAXSAMPLES) {
-      return AHI_NOSOUND;
-    }
-  }
-
-  file = Open(filename, MODE_OLDFILE);
-  
-  if(file) {
-    int length;
-
-    Seek(file, 0, OFFSET_END);
-    length = Seek(file, 0, OFFSET_BEGINNING);
-    *samplearray = AllocVec(length, MEMF_PUBLIC);
-    if(*samplearray) {
-      Read(file, *samplearray, length);
-
-      sample.ahisi_Type = type;
-      sample.ahisi_Address = *samplearray;
-#if USE_AHI_V4
-      sample.ahisi_Length = length / AHI_SampleFrameSize(type);
-#else
-      sample.ahisi_Length = length / (type == AHIST_M16S ? 2 : 1);
-#endif
-      if(! AHI_LoadSound(id, AHIST_SAMPLE, &sample, actrl)) {
-        rc = id;
-      }
-    }
-    Close(file);
-  }
-  return rc;
-}
-
-
-/******************************************************************************
-**** UnloadSample *************************************************************
-******************************************************************************/
-
-void UnloadSample(UWORD id) {
-
-  AHI_UnloadSound(id, actrl);
-  FreeVec(samples[id]);
-  samples[id] = NULL;
-}
-
-/******************************************************************************
-**** main *********************************************************************
-******************************************************************************/
-
-int main() {
-
-        // Start feeding samples to sound hardware
-        if(!(AHI_ControlAudio(actrl, AHIC_Play, TRUE, TAG_DONE))) {
-
-          AHI_Play(actrl,
-            // A forever-looping sample on channel 0
-            AHIP_BeginChannel,  0,
-            AHIP_Freq,          17640,
-            AHIP_Vol,           0x10000,
-            AHIP_Pan,           0xc000,
-            AHIP_Sound,         louise,
-            AHIP_EndChannel,    NULL,
-
-            // A oneshot sample on channel 1
-            AHIP_BeginChannel,  1,
-            AHIP_Freq,          22254,
-            AHIP_Vol,           0x10000,
-            AHIP_Pan,           0x4000,
-            AHIP_Sound,         ahem,
-            AHIP_LoopSound,     AHI_NOSOUND,
-            AHIP_EndChannel,    NULL,
-
-            TAG_DONE);
-
-
-          channelstate[0].Volume   = 0x10000;
-          channelstate[0].Position = 0xc000;
-          channelstate[1].Volume   = 0x10000;
-          channelstate[1].Position = 0x4000;
-
-          // Wait 5 seconds
-          
-          Delay(5 * TICKS_PER_SECOND);
-
-
-        }
-      }
-    }
-  }
-
-  return 0;
-}
-
-#endif
