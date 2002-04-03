8484a6cfc1b14940ee045a100c9a116ed024a069
Thomas Akesson <tapilot@home.se>
2002-04-03 22:19:54 +0000

    New version of agi2pdb that complies with the new PalmOS database specification. Not quite finished though...

--- src/utils/agi2pdb/agi2pdb.c
+++ src/utils/agi2pdb/agi2pdb.c
@@ -10,435 +10,147 @@
 #undef VERSION
 #endif
 
-#define VERSION "0.0.1"
+#define VERSION "0.2"
 
-int test_v3_game(char *dirfile, char *gpath);
-int test_v2_game(char *gpath);
-void copy_file(FILE *ifp, FILE *ofp);
-void do_pdbheader(void);
-int convert_v2(char *outfile, char *gpath);
-int convert_v3(char *outfile, char *dirfile, char *gpath);
 
-struct PalmOSAGI palmagi;
-struct pdb_recheader rh;
-struct pdb_pdbheader PDBHeader;
+#ifndef MACOSX_SDL
+volatile UINT32 clock_ticks;
+volatile UINT32 clock_count;
+#endif
+
 struct sarien_options opt;
+struct agi_game game;
+
+extern struct agi_loader *loader;
+
+extern struct agi_loader agi_v2;
+extern struct agi_loader agi_v3;
+
+
+
+void DieWithError(char *errstr, Err err) {
+	printf("%s", errstr);
+	if (err)
+		printf(" (%x )\n", err);
+	else
+		printf("\n");
+	exit(1);
+}
 
 int main(int argc, char *argv[])
 {
-	printf("agi2pdb v" VERSION " :: Converting Sierra AGI files to PalmOS Databases\n"
-		"(C) Copyright 2001 Stuart George\n"
+	char *dir = ".";
+	char *pdb = NULL;
+	int ec;
+
+	printf("agi2pdb v" VERSION " - Converting Sierra AGI files to PalmOS Database\n"
+		"(C) Copyright 2001-2002 Stuart George & Thomas Akesson\n"
 		"\n"
 		"This program is free software; you can redistribute it and/or modify it\n"
 		"under the terms of the GNU General Public License, version 2 or later,\n"
-		"as published by the the Free Software Foundation.\n");
+		"as published by the the Free Software Foundation.\n"
+		"\n");
 
-	if(argc<3)
-	{
-		printf("syntax;\n"
-			"For AGI v2 games;\n"
-			"  agi2pdb game.pdb {directory}\n"
-			"For AGI v3 games;\n"
-			"  agi2pdb game.pdb DIRFILE {directory}\n"
-			"\n"
-			"eg:\n"
-			"agi2pdb KingsQuestII.pdb /games/kq2\n"
-			"agi2pdb KingsQuestIII.pdb\n"
-			"agi2pdb GoldRush.pdb GRDIR /games/gr\n"
-			"agi2pdb BlackCauldron.pdb BCDIR /games/bc/2.10\n");
+	if(argc>3 || (argc==2 && argv[1][1]=='?')) {
+		printf("syntax:\n"
+			"  agi2pdb {directory} {game.pdb}\n");
+		return 0;
 	}
-	else
-	{
-		strcpy(exec_name, argv[0]);
-
-    	if(test_v3_game(argv[2], argv[3])==err_OK)
-    	{
-    		convert_v3(argv[1], argv[2], argv[3]);
-    	}
-    	else
-    	{
-    		if(test_v2_game(argv[2])==err_OK)
-    		{
-    			convert_v2(argv[1], argv[2]);
-    		}
-    	}
+	else if (argc>1){
+		dir = argv[1];
+		if (argc>2)
+			pdb = argv[2];
 	}
 
+	ec = agi_detect_game (dir);
+
+	if (ec == err_OK) {
+		if (loader == &agi_v2)
+			printf("AGI version 2 game detected.\n");
+		else
+			printf("AGI version 3 game detected.\n");
+
+
+		if (pdb == NULL) {
+			pdb = (char*) malloc(32);
+			match_crc_name (game.crc, get_config_file(), pdb);
+		}
+		if (game.ver) {
+			agi_init();
+			convert2pdb (loader, pdb);
+		}
+		else {
+			printf("Game version could not be determined.\n"
+			"Make sure Sarien configuration file is available!\n");
+		}
+	}
+	else {
+		printf("No AGI game detected.\n");
+		return 1;
+	}
 	return 0;
 }
 
-int test_v3_game(char *dirfile, char *gpath)
+
+/* Well, this is a hack to get the name from the conf file */
+UINT32 match_crc_name (UINT32 crc, char *path, char *game_name)
 {
-	FILE *fp;
-	int rc=err_Unk;
-	char *p;
-
-	if(gpath!=NULL)
-		strcpy(dir, gpath);
-	else
-		strcpy(dir, "./");
-
-	fixpath(NO_GAMEDIR, dirfile);
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		fclose(fp);
-
-		/* valid dir file */
-
-		p=strchr(dirfile, 0x0);
-		p--;
-
-		if(p>dirfile && tolower(*p--)=='r')
-		{
-		    if(p>dirfile && tolower(*p--)=='i')
-		    {
-		        if(p>dirfile && tolower(*p--)=='d')
-		        {
-		        	rc=err_OK;
-		        }
-		    }
-		}
-	}
-
-	return rc;
-}
-
-int test_v2_game(char *gpath)
-{
-	char *fnames[]={"logdir", "picdir"};
-	int i;
-	int rc=err_OK;
-	FILE *fp;
-
-	if(gpath!=NULL)
-		strcpy(dir, gpath);
-	else
-	    strcpy(dir, "./");
-
-	for(i=0; rc==err_OK && i<2; i++)
-	{
-		fixpath(NO_GAMEDIR, fnames[i]);
-		if((fp=fopen(path, "rb"))!=NULL)
-			fclose(fp);
-		else
-			rc=err_Unk;
-	}
-
-	return rc;
-}
-
-void copy_file(FILE *ifp, FILE *ofp)
-{
-	char *buff;
+	FILE *f;
+	char *c, *t, buf[256];
+	UINT32 id, ver;
 	int len;
 
-	buff=(char*)malloc(8192);
+	if ((f = fopen (path, "r")) == NULL)
+		return 0;
 
-	len=1;
+	while (!feof (f)) {
+		fgets (buf, 256, f);
+		c = strchr (buf, '#');
+		if (c) *c = 0;
 
-	do
-	{
-		len=fread(buff, 1, 8192, ifp);
-		if(len>0)
-			fwrite(buff, 1, len, ofp);
-	}while(len>0);
+		/* Remove spaces at end of line */
+		if (strlen (buf)) {
+			for (c = buf + strlen (buf) - 1;
+				*c == ' ' || *c == '\t'; *c-- = 0) {}
+		}
 
-	free(buff);
-}
 
-void do_pdbheader(void)
-{
-	memset(&PDBHeader, 0x0, sizeof(struct pdb_pdbheader));
+		t = strtok (buf, " \t\r\n");
+		if (t == NULL)
+			continue;
+		id = strtoul (t, NULL, 0);
 
-	/* db name in palm */
-	strncpy(PDBHeader.name, game_name, 32);
+		t = strtok (NULL, " \t\r\n");
+		if (t == NULL)
+			continue;
+		ver = strtoul (t, NULL, 0);
 
-	/* can install over existing pdb */
-	write_word((UINT8*)&PDBHeader.fileAttributes, 0x10);
-	write_word((UINT8*)&PDBHeader.version, 0x0001);
-	write_dword((UINT8*)&PDBHeader.creationDate, 1);
-	write_dword((UINT8*)&PDBHeader.modificationDate, 1);
+		t = strtok (NULL, "(\n\r");
 
-	/* AGI v4 PalmOS DB File */
-	PDBHeader.databaseType[0]='a';
-	PDBHeader.databaseType[1]='g';
-	PDBHeader.databaseType[2]='i';
-	PDBHeader.databaseType[3]='4';
+		if (id == crc) {
 
-	/* Our registered Palm Creator ID */
-	PDBHeader.creatorID[0]='F';
-	PDBHeader.creatorID[1]='A';
-	PDBHeader.creatorID[2]='G';
-	PDBHeader.creatorID[3]='I';
-}
 
-int convert_v2(char *outfile, char *gpath)
-{
-	FILE *ofp;
-	FILE *fp;
-	char	buff[256];
-	/* int		len; */
-	int		i;
-	int		total_len;
+			if (*t == '[') {
+				while (*t != ']') {
+					t++;
+				}
+				t++;
 
-	printf("\nConverting a v2 game file to PalmOSDB v4\n");
-	ofp=fopen("$$temp$$.tmp", "wb+");
+				for (; *t == ' ' || *t == '\t'; t++) {}
+			}
 
-	memset(&palmagi, 0x0, sizeof(struct	PalmOSAGI));
-
-	/* does version + crc */
-	v2id_game();
-	strncpy(palmagi.name, game_name, 32);
-
-	if(opt.agds)
-		palmagi.options|=OPT_AGDS;
-	if(opt.amiga)
-		palmagi.options|=OPT_AMIGA;
-
-	memset(buff, 0x0, 256);
-
-	/* 256 byte header */
-	fwrite(buff, 1, 256, ofp);
-
-	fixpath(NO_GAMEDIR,	"logdir");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v2.logdir[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v2.logdir[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR,	"picdir");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v2.picdir[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v2.picdir[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR,	"viewdir");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v2.viewdir[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v2.viewdir[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR,	"snddir");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v2.snddir[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v2.snddir[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR,	"object");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v2.object[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v2.object[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR,	"words.tok");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v2.words[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v2.words[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	for(i=0; i<16; i++)
-	{
-		sprintf(buff, "vol.%i", i);
-		fixpath(NO_GAMEDIR,	buff);
-		if((fp=fopen(path, "rb"))!=NULL)
-		{
-			printf("Inserting %s\n", path);
-			palmagi.v4.v2.vol[i*2]=ftell(ofp);
-			copy_file(fp, ofp);
-			palmagi.v4.v2.vol[1+(i*2)]=ftell(fp);
-			fclose(fp);
+			fclose (f);
+			len = strlen(t);
+			if (t[len-1] == ' ')
+				len--;
+			if (len > 31)
+				len = 31;
+			strncpy(game_name, t, len);
+			game_name[len] = '\0';
+			return ver;
 		}
 	}
 
-	fseek(ofp, 0x0, SEEK_END);
-	total_len=ftell(ofp);
-
-	/* write good header */
-	fseek(ofp, 0x0, SEEK_SET);
-	fwrite(&palmagi, 1, sizeof(struct PalmOSAGI), ofp);
-
-	/* now build the real palm PDB file */
-	fp=fopen(outfile, "wb+");
-	if(fp!=NULL)
-	{
-		/* fill in Palm PDB Header */
-	 	do_pdbheader();
-
-		/* skip pdbheader size */
-		fseek(fp, sizeof(struct pdb_pdbheader), SEEK_SET);
-
-		total_len=(total_len + 4095) & 0xfffff000;
-		write_word((UINT8*)&PDBHeader.numberOfRecords, total_len/4096);
-
-		/* create record headers */
-		for(i=0; i<total_len/4096; i++)
-		{
-			memset(&rh, 0x0, sizeof(struct pdb_recheader));
-			write_dword((UINT8*)&rh.recordDataOffset, (sizeof(struct pdb_pdbheader)) + (total_len/4096 * sizeof(struct pdb_recheader)) + i*4096);
-			rh.recordAttributes=0x60;
-			fwrite(&rh, 1, sizeof(struct pdb_recheader), fp);
-		}
-
-		/* copy created pdb */
-		fseek(ofp, 0x0, SEEK_SET);
-		copy_file(ofp, fp);
-
-		/* write good PDB Header */
-		fseek(fp, 0x0, SEEK_SET);
-		fwrite(&PDBHeader, 1, sizeof(struct pdb_pdbheader), fp);
-
-		fclose(fp);
-	}
-
-	fclose(ofp);
-
-	remove("$$temp$$.tmp");
-
+	fclose (f);
 	return 0;
-}
-
-int convert_v3(char *outfile, char *dirfile, char *gpath)
-{
-	FILE *ofp;
-	FILE *fp;
-	char	buff[256];
-	/* int		len; */
-	int		i;
-	int		total_len;
-
-	printf("\nConverting a v3 game file to PalmOSDB v4\n");
-	ofp=fopen("$$temp$$.tmp", "wb+");
-
-	memset(&palmagi, 0x0, sizeof(struct	PalmOSAGI));
-
-	strncpy(name, dirfile, strlen(dirfile)-3);
-
-	/* does version + crc */
-	v3id_game();
-	strncpy(palmagi.name, game_name, 32);
-
-	if(opt.agds)
-		palmagi.options|=OPT_AGDS;
-	if(opt.amiga)
-		palmagi.options|=OPT_AMIGA;
-
-	memset(buff, 0x0, 256);
-
-	/* 256 byte header */
-	fwrite(buff, 1, 256, ofp);
-
-	fixpath(NO_GAMEDIR,	dirfile);
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v3.dir[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v3.dir[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR, "object");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v3.object[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v3.object[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	fixpath(NO_GAMEDIR, "words.tok");
-	if((fp=fopen(path, "rb"))!=NULL)
-	{
-		printf("Inserting %s\n", path);
-		palmagi.v4.v3.words[0]=ftell(ofp);
-		copy_file(fp, ofp);
-		palmagi.v4.v3.words[1]=ftell(fp);
-		fclose(fp);
-	}
-
-	for(i=0; i<16; i++)
-	{
-		sprintf(buff, "vol.%i", i);
-		fixpath(GAMEDIR, buff);
-		if((fp=fopen(path, "rb"))!=NULL)
-		{
-			printf("Inserting %s\n", path);
-			palmagi.v4.v3.vol[i*2]=ftell(ofp);
-			copy_file(fp, ofp);
-			palmagi.v4.v3.vol[1+(i*2)]=ftell(fp);
-			fclose(fp);
-		}
-	}
-
-	fseek(ofp, 0x0, SEEK_END);
-	total_len=ftell(ofp);
-
-	/* write good header */
-	fseek(ofp, 0x0, SEEK_SET);
-	fwrite(&palmagi, 1, sizeof(struct PalmOSAGI), ofp);
-
-	/* now build the real palm PDB file */
-	fp=fopen(outfile, "wb+");
-	if(fp!=NULL)
-	{
-		/* fill in Palm PDB Header */
-	 	do_pdbheader();
-
-		/* skip pdbheader size */
-		fseek(fp, sizeof(struct pdb_pdbheader), SEEK_SET);
-
-		total_len=(total_len + 4095) & 0xfffff000;
-		write_word((UINT8*)&PDBHeader.numberOfRecords, total_len/4096);
-
-		/* create record headers */
-		for(i=0; i<total_len/4096; i++)
-		{
-			memset(&rh, 0x0, sizeof(struct pdb_recheader));
-			write_dword((UINT8*)&rh.recordDataOffset, (sizeof(struct pdb_pdbheader)) + (total_len/4096 * sizeof(struct pdb_recheader)) + i*4096);
-			rh.recordAttributes=0x60;
-			fwrite(&rh, 1, sizeof(struct pdb_recheader), fp);
-		}
-
-		/* copy created pdb */
-		fseek(ofp, 0x0, SEEK_SET);
-		copy_file(ofp, fp);
-
-		/* write good PDB Header */
-		fseek(fp, 0x0, SEEK_SET);
-		fwrite(&PDBHeader, 1, sizeof(struct pdb_pdbheader), fp);
-
-		fclose(fp);
-	}
-
-	fclose(ofp);
-
-	remove("$$temp$$.tmp");
-
-	return 0;
-
 }
--- src/utils/agi2pdb/agi2pdb.h
+++ src/utils/agi2pdb/agi2pdb.h
@@ -2,246 +2,47 @@
 #ifndef __AGI2PDB_H
 #define __AGI2PDB_H
 
-#include <stdlib.h>
-
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-typedef unsigned char	UINT8;
-typedef unsigned short	UINT16;
-#ifndef WIN32
-typedef unsigned long	UINT32;
-#endif
-typedef signed char	SINT8;
-typedef signed short	SINT16;
-typedef signed long	SINT32;
+#include "sarien.h"
+#include "agi.h"
+#include "palmdb.h"
 
-#ifndef FALSE
-#define FALSE		0
-#define TRUE		(!FALSE)
-#endif
 
-#ifndef MAX_PATH
-#define MAX_PATH	256
-#endif
+void write_word(UINT8 *mem, UINT16 w);
+void write_dword(UINT8 *mem, UINT32 w);
+void DieWithError(char *errstr, Err err);
 
-extern	char	path[];
-extern	char	dir[];
-extern	char	name[];
-extern	char	game_name[];
-extern	char	exec_name[];
-extern	char	*out_file;
-extern	struct PalmOSAGI palmagi;
+int convert2pdb (struct agi_loader *loader, char *pdbname);
+int convertDir(struct agi_dir *agid, UINT16 dir[], UINT16 *index);
+Err convertObjects(PalmDBRecord rec, UINT16 num_objects);
+UINT32 match_crc_name (UINT32 crc, char *path, char *game_name);
 
-#define	OPT_AGDS		0x0001
-#define OPT_AMIGA		0x0002
 
-struct sarien_options {
-	int forceload;		/* force loading of all resources */
-	int cache;		/* cache loaded resources */
-	int gamerun;		/* status for game */
-	int pcjrgfx;		/* PCJr graphics */
-	int showscreendraw;	/* show screen drawing */
-	int showkeypress;
-	int emuversion;
-#ifndef NO_DEBUG
-	int debug;
-#endif
-	int scale;		/* window scale factor */
-	int agds;		/* enable AGDS mode */
-	int amiga;		/* enable Amiga mode */
-	int fullscreen;		/* use full screen mode if available */
-	int nosound;		/* disable sound */
-#ifdef MITSHM
-	int mitshm;		/* use shared memory extension */
-#endif
-#ifdef XF86DGA
-	int dga;		/* use XFree86 DGA extension */
-#endif
-	int soundemu;		/* sound emulation mode */
+#define sndTypeAGI 1
+#define sndTypeSMF0 2
+#define sndTypeSMF1 3
+struct agi_v4_sndInfo {
+			  UINT8 sndType;
+			  UINT8 sndChannels; /* Valid range: 0..4 */
 };
 
-extern struct sarien_options opt;
-extern char *fixpath (int flag, char *fname);
-
-enum error {
-	err_OK = 0,
-	err_DoNothing,
-	err_BadCLISwitch,
-	err_InvalidAGIFile,
-	err_BadFileOpen,
-	err_NotEnoughMemory,
-	err_BadResource,
-	err_UnknownAGIVersion,
-	err_RestartGame,
-	err_NoLoopsInView,
-	err_ViewDataError,
-	err_NoGameList,
-
-	err_Unk = 127
-};
-
-enum {
-	NO_GAMEDIR = 0,
-	GAMEDIR
-};
-
-#define DIR_		"dir."
-
-#define LOGDIR		"logdir"
-#define PICDIR		"picdir"
-#define VIEWDIR		"viewdir"
-#define	SNDDIR		"snddir"
-#define OBJECTS		"object"
-#define WORDS		"words.tok"
-
-#ifdef _TRACE
-#include <stdio.h>
-#define _D(args...) do { \
-        printf("\x1b[33m" __PRETTY_FUNCTION__ " \x1b[37m[" __FILE__ \
-        ":%d] " _D_INFO, __LINE__); printf (args); printf ("\x1b[0m\n"); \
-        } while (0)
-#else
-#ifdef _D
-#undef _D
-#endif
-
-void _D(char *, ...);
-#endif
-
-extern void write_word(UINT8 *mem, UINT16 w);
-extern void write_dword(UINT8 *mem, UINT32 w);
-
-
-struct pdb_pdbheader
-{
-	UINT8 name[32];		// String - This is the name of the database
-				//  on the PalmPilot device.
-				//  It need not match the name of the PDB file
-				//  in the environment in which it is created.
-	UINT16 fileAttributes;	// Attributes of the pdb file.
-				//  0x0002 Read-Only
-				//  0x0004 Dirty AppInfoArea
-				//  0x0008 Backup this database
-				//         (i.e. no conduit exists)
-				//  0x0010 Okay to install newer over existing
-				//         copy, if present on PalmPilot
-				//  0x0020 Force the PalmPilot to reset after
-				//         this database is installed
-	UINT16 version;		// Defined by the application.
-	UINT32 creationDate;	// Expressed as the number of seconds since
-				//  January 1, 1904. The database will not
-				//  install if this value is zero. (PalmOS
-				//  1.0.6)
-	UINT32 modificationDate;// Expressed as the number of seconds since
-				//  January 1, 1904.
-				//  The database will not install if this
-				//  value is zero. (PalmOS 1.0.6)
-	UINT32 lastBackupDate;	// Expressed as the number of seconds since
-				//  January 1, 1904. This can be left at zero
-				//  and the database will install.
-	UINT32 modificationNumber;// Set to zero.
-	UINT32 appInfoArea;	// The byte number in the PDB file (counting
-				//  from zero) at which the AppInfoArea is
-				//  located. This must be the first entry
-				//  in the Data portion of the PDB file.
-				//  If this database does not have an
-				//  AppInfoArea, set this value to zero.
-	UINT32 sortInfoArea;	// The byte number in the PDB file (counting
-				//  from zero) at which the SortInfoArea is
-				//  located. This must be placed immediately
-				//  after the AppInfoArea, if one exists,
-				//  within the Data portion of the PDB file.
-				//  If this database does not have a
-				//  SortInfoArea, set this value to zero.
-				//  Do not use this. See Note C below for
-				//  further details.
-	UINT8 databaseType[4];	// String - Set this to the desired value.
-				//  Generally it should match the Database Type
-				//  used by the corresponding application.
-				//  This is 4 characters long and does not have
-				//  a terminating null.
-	UINT8 creatorID[4];	// String - Set this to the desired value.
-				//  Generally it should match the Creator ID
-				//  used by the corresponding application. In
-				//  all cases, you should always register your
-				//  Creator ID before using it. This is 4
-				//  characters long and does not have a
-				//  terminating null.
-	UINT32 uniqueIDSeed;	// This is used to generate the Unique ID
-				//  number of subsequent records. Generally,
-				//  this should be set to zero.
-	UINT32 nextRecordListID;// Set this to zero. (This does not appear
-				//  to be used, but that has not been
-				//  verified by a third party.)
-	UINT16 numberOfRecords;	// This contains the number of records
+struct agi_v4_header {
+	UINT16  format;  /* Type of the database. Currently 0 */
+	UINT16  revision; /* Revision number of the database format, currently 0 */
+	char    id[8]; /* Game ID in order to easily clean up (savegames) when deleting a game. */
+	UINT16  version; /* Interpreter version used by the game */
+	UINT16  emulation; /* Interpreter version to emulate */
+	UINT32  options; /* For Amiga/AGDS, etc (it seems that the current agi_v4 is independent of those)*/
+	struct agi_v4_sndInfo sndInfo;
 }
 #ifdef __GNUC__
 __attribute__((packed))
 #endif
 ;
 
-struct pdb_recheader{
-	UINT32 recordDataOffset;// The byte number in the PDB file (counting
-				//  from zero) at which the record is located.
-	UINT8 recordAttributes;	// The records attributes.
- 				//  0x10 Secret record bit.
-				//  0x20 Record in use (busy bit).
-				//  0x40 Dirty record bit.
-				//  0x80 Delete record on next HotSync.
-				//  The least significant four bits are used
-				//   to represent the category values.
-	UINT8	uniqueID[3];	// Set this to zero and do not try to
-				//  second-guess what PalmOS will do with
-				//  this value.
-}
-#ifdef __GNUC__
-__attribute__((packed))
-#endif
-;
-
-
-struct PalmOSAGI2
-{
-	UINT32	logdir[2];
-	UINT32	picdir[2];
-	UINT32	viewdir[2];
-	UINT32	snddir[2];
-
-	UINT32	object[2];
-	UINT32	words[2];
-
-	UINT32	vol[16*2];		// 0=offs, 1=len
-};
-
-struct PalmOSAGI3
-{
-	UINT32	dir[2];
-	UINT32	object[2];
-	UINT32	words[2];
-	UINT32	vol[16*2];		// 0=offs, 1=len
-};
-
-union PalmOSAGI4
-{
-	struct PalmOSAGI2 v2;
-	struct PalmOSAGI3 v3;
-};
-
-struct PalmOSAGI
-{
-	char	name[32];
-	UINT16	version;
-	UINT16	emulation;
-	UINT32	options;
-	UINT32	crc;
-
-	union PalmOSAGI4 v4;
-};
-
-int v2id_game (void); 
-int v3id_game (void);
 
 #ifdef __cplusplus
 };
