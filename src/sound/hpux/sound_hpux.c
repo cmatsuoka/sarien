cbca146d0da1e01a54c571a841e3b9b2c9b38989
Claudio Matsuoka <cmatsuoka@gmail.com>
2002-05-13 00:20:09 +0000

    Added some HP-UX sound code. We're hard-wired to 16-bit, 22-kHz mono.

--- src/sound/hpux/sound_hpux.c
+++ src/sound/hpux/sound_hpux.c
@@ -24,6 +24,7 @@ static struct sound_driver sound_hpux = {
 	hpux_dump_buffer
 };
 
+static int fd;
 
 
 void __init_sound ()
@@ -34,15 +35,68 @@ void __init_sound ()
 
 static int hpux_init_sound (SINT16 *b)
 {
+	int flags;
+	int gain = 128;
+	int bsize = 32 * 1024;
+	int port = AUDIO_OUT_SPEAKER;
+	struct audio_gains agains;
+	struct audio_describe adescribe;
+
+	if ((flags = fcntl (fd, F_GETFL, 0)) < 0)
+		return -1;
+
+	flags |= O_NDELAY;
+	if ((flags = fcntl (fd, F_SETFL, flags)) < 0)
+		return -1;
+
+	if (ioctl (fd, AUDIO_SET_DATA_FORMAT, AUDIO_FORMAT_LINEAR16BIT) == -1)
+		return -1;
+
+	if (ioctl (fd, AUDIO_SET_CHANNELS, 1) == -1)
+		return -1;
+
+	if (ioctl (fd, AUDIO_SET_OUTPUT, port) == -1)
+		return -1;
+
+	if (ioctl (fd, AUDIO_SET_SAMPLE_RATE, 22050) == -1)
+		return -1;
+
+	if (ioctl (fd, AUDIO_DESCRIBE, &adescribe) == -1)
+		return -1;
+
+	if (ioctl (fd, AUDIO_GET_GAINS, &agains) == -1)
+		return -1;
+
+	agains.transmit_gain = adescribe.min_transmit_gain +
+		(adescribe.max_transmit_gain - adescribe.min_transmit_gain) *
+		gain / 256;
+
+	if (ioctl (fd, AUDIO_SET_GAINS, &agains) == -1)
+		return -1;
+
+	if (ioctl(fd, AUDIO_SET_TXBUFSIZE, bsize) == -1)
+		return -1;
+
+	return 0;
 }
 
 
 static void hpux_close_sound ()
 {
+	close (fd);
 }
 
 
 static void dump_buffer ()
 {
+	int i = BUFFER_SIZE << 1, j;
+	SINT16 *b = buffer;
+
+	do {
+		if ((j = write (audio_fd, b, i)) > 0) {
+			i -= j;
+			b += j;
+		}
+	} while (i);
 }
 
