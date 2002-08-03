44958e5a885177ba807543d3061b3bd818acd843
Claudio Matsuoka <cmatsuoka@gmail.com>
2002-08-03 16:05:38 +0000

    Fixed KQ4 left screen border detection (v4 is set when x <= 0, other interpreter versions use x < 0). closes: #590462

--- src/core/checks.c
+++ src/core/checks.c
@@ -193,6 +193,9 @@ void update_position ()
 		if (x < 0) {
 			x = 0;
 			border = 4;
+		} else if (x <= 0 && agi_get_release() == 0x3086) {  /* KQ4 */
+			x = 0;				 /* See bug #590462 */
+			border = 4;
 		} else if (v->entry == 0 && x == 0 && v->flags & ADJ_EGO_XY) {
 			/* Extra test to walk west clicking the mouse */
 			x = 0;
